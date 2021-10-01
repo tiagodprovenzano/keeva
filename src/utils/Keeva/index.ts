0c0475181b15f08ce0f5f326dec6b0578de50102
Tiago <31420080+tiagodprovenzano@users.noreply.github.com>
2021-10-01 15:53:25 -0300

    reading keevarc

--- src/utils/Keeva/index.ts
+++ src/utils/Keeva/index.ts
@@ -1,12 +1,17 @@
 import * as fs from "fs";
 import path from "path";
 
+type ICustomConfig = {
+    templatesUri: string
+}
+
 export abstract class Keeva {
   protected name: string;
   protected dirPath: string;
   protected workspaceFolderPath: string | undefined;
   protected commandDir: string;
-  
+  protected customConfig: ICustomConfig | null;
+
   constructor(
     name: string,
     dirPath: string,
@@ -17,16 +22,36 @@ export abstract class Keeva {
     this.dirPath = dirPath;
     this.workspaceFolderPath = workspaceFolderPath;
     this.commandDir = commandDir;
+    const hasCustomConfig = this.hasConfigFile()
+    if(hasCustomConfig && this.workspaceFolderPath){
+        this.customConfig = require(path.join(this.workspaceFolderPath, ".keevarc"));
+        console.log(this.customConfig);
+    }else{
+        this.customConfig = null
+    }
   }
 
-  protected hasCustomTemplates = () => {
+  protected hasConfigFile = (): boolean => {
     if (this.workspaceFolderPath) {
+      const configFile = path.join(this.workspaceFolderPath, ".keevarc");
+      try {
+        fs.readFileSync(configFile);
+      } catch (error) {
+        return false;
+      }
+      return true;
+    }
+    return false;
+  };
+
+  protected hasCustomTemplates = () => {
+    if (this.workspaceFolderPath && this.customConfig?.templatesUri) {
       const customTemplatesPath = path.join(
         this.workspaceFolderPath,
-        ".keeva",
-        "templates",
+        this.customConfig?.templatesUri,
         this.commandDir
       );
+      console.log({customTemplatesPath});
       try {
         fs.readdirSync(customTemplatesPath);
       } catch (error) {
