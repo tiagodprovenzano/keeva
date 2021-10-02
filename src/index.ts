d9b89507c79a45f113ff631191a531b7c9bfb7e7
Tiago <31420080+tiagodprovenzano@users.noreply.github.com>
2021-10-02 08:50:14 -0300

    export create function

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,24 @@
 import { camelToKebab } from "./utils/library/camelToKebab";
 import { Component } from "./utils/Component";
-import { Template } from "./utils/Template";
-import { getVariablesFromInputText } from "./utils/library/getVariablesFromInputText";
 import { ConfigFile } from "./utils/CofigFIle";
 import { Variables } from "./utils/Variables";
 import { Command } from "./utils/Command";
-import { Text } from "./utils/Text";
 
-export { camelToKebab, Component };
-
-const variables = {
-  name: "PanelFilter",
-};
-const pathToJson = "/home/tiago/Repos/TestingKeeva/keeva.config.json";
-
-const inputText = "name=LothalWarrior address=CrazyStyles";
-
-function input(
+function create(
   projectPath: string,
   targetPath: string,
   command: string,
   variablesInput: string
-) {
+): void {
   const configFile = new ConfigFile(projectPath);
   const variables = new Variables(variablesInput).variables;
   const commandDir = new Command(command).folder;
-  console.log({
-    projectPath,
-    targetPath,
-    command,
-    variablesInput,
-    templatesUri: configFile.templateUri,
-    methods: configFile.methods,
+  new Component(
     variables,
-  });
-  new Component(variables, targetPath, configFile.templateUri, commandDir).create();
+    targetPath,
+    configFile.templateUri,
+    commandDir
+  ).create();
 }
 
-input(
-  "/home/tiago/Repos/TestingKeeva/",
-  "/home/tiago/Repos/PloomesCRMWeb/src/Keeva",
-  "Component",
-  inputText
-);
+export { camelToKebab, create };
